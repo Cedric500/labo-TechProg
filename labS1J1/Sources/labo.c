7aedcac9eb259795ec1d2340e63d159df8e9e0d8
Cedric Hayfield <1730001@collegebart.ca>
2025-01-31 13:34:09 -0500

    Ajout des fonctions

--- labS1J1/Sources/labo.c
+++ labS1J1/Sources/labo.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 #include "labo.h"
 
+char* sousTexte(char* texte, int from, int to)
+{
+
+	return nullptr;
+}
+
+void set_String(char* text, String* s)
+{
+
+}
+
+int String_is_palindrome(String* s)
+{
+
+	return 0;
+}
+
+void bubbleSort(int elements[], int n)
+{
+
+}
